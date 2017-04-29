3810a7d582b774042e97d90cda355e7bcf4e02e2
Edward James Moore <edmoore7@gmail.com>
2017-04-28 22:43:05 -0500

    print none if no comment

--- logbookentry.py
+++ logbookentry.py
@@ -8,7 +8,7 @@ class LogbookEntry(object):
 class OtrLogbookEntry(LogbookEntry):
     def __init__(self, entry_date, entry_time, otr_comments):
         super(OtrLogbookEntry, self).__init__(entry_date, entry_time)
-        self.otr_comments = otr_comments if otr_comments else '<none>'
+        self.otr_comments = otr_comments
 
 class OtrGlucoseEntry(OtrLogbookEntry):
     def __init__(self, entry_date, entry_time, otr_comments, bg_value):
@@ -20,7 +20,7 @@ class OtrGlucoseEntry(OtrLogbookEntry):
             self.entry_date.strftime('%m/%d/%Y'),
             self.entry_time.strftime('%I:%M %p'),
             self.bg_value,
-            self.otr_comments
+            self.otr_comments if self.otr_comments else '<none>'
             ))
 
 class OtrPatternEntry(OtrLogbookEntry):
@@ -31,5 +31,5 @@ class OtrPatternEntry(OtrLogbookEntry):
     def __str__(self):
         return format('%s, %s' % (
             self.entry_date.strftime('%m/%d/%Y'),
-            self.otr_comments
+            self.otr_comments if self.otr_comments else '<none>'
             ))
